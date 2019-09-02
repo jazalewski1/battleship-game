2efe703c0194e1c287859c773e4b15c0df69371d
Jakub Zalewski <jazalewski1@gmail.com>
2019-09-02 15:45:25 +0200

    Add ship fill function

--- src/players.hpp
+++ src/players.hpp
@@ -19,6 +19,7 @@ class Player : public sf::Drawable
 		const BoardGrid* m_defenseGrid;
 
 		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
+		virtual void fillShips() = 0;
 
 
 	public:
@@ -29,12 +30,17 @@ class Player : public sf::Drawable
 
 		bool isShip(sf::Vector2i index)
 		{
+			index = index;
+			return true;
 		}
 };
 
 
 class HumanPlayer : public Player
 {
+	private:
+		const BoardGrid* m_placeGrid;
+
 	private:
 		void draw(sf::RenderTarget& target, sf::RenderStates states) const
 		{
@@ -44,9 +50,19 @@ class HumanPlayer : public Player
 				target.draw(marker.second);
 		}
 
+		void fillShips()
+		{
+			sf::Vector2i start {dynamic_cast<const BoardGrid*>(m_placeGrid)->getBounds()};
+			m_ships.push_back(ShipGrid{start.x, start.y + 0, 5});
+			m_ships.push_back(ShipGrid{start.x, start.y + 1, 4});
+			m_ships.push_back(ShipGrid{start.x, start.y + 2, 3});
+			m_ships.push_back(ShipGrid{start.x, start.y + 3, 3});
+			m_ships.push_back(ShipGrid{start.x, start.y + 4, 2});
+		}
+
 	public:
-		HumanPlayer(const BoardGrid* attackGrid, const BoardGrid* defenseGrid) :
-			Player{attackGrid, defenseGrid}
+		HumanPlayer(const BoardGrid* attackGrid, const BoardGrid* defenseGrid, const BoardGrid* placeGrid) :
+			Player{attackGrid, defenseGrid}, m_placeGrid{placeGrid}
 		{
 		}
 
